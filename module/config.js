d0701aed4dd5d2cb3686bcbbe4b5091b6a3378f9
Paul C <pacosgrove1@googlemail.com>
2021-08-11 01:19:38 +0100

    begin layout for actor sheets, add more translations

--- module/config.js
+++ module/config.js
@@ -12,3 +12,12 @@ legends.approaches = {
   "advance-attack": "legends.techniques.approaches.advance-attack",
   "evade-observe": "legends.techniques.approaches.evade-observe"
 };
+
+legends.training = {
+  "water": "legends.training.water",
+  "fire": "legends.training.fire",
+  "earth": "legends.training.earth",
+  "air": "legends.training.air",
+  "weapons": "legends.training.weapons",
+  "technology": "legends.training.technology"
+}
